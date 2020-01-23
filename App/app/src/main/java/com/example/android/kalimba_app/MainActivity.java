3b16a7745da4396419984c008371965e14e81bd4
Eugene <eugenetdr@gmail.com>
2020-01-23 14:54:50 +0800

    Adds BT device search

--- App/app/src/main/java/com/example/android/kalimba_app/MainActivity.java
+++ App/app/src/main/java/com/example/android/kalimba_app/MainActivity.java
@@ -2,13 +2,90 @@ package com.example.android.kalimba_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    ListView listView;
+    private Button button1;
+    HashMap<String, String> nameList = new HashMap<String, String>();
+    ArrayList<String> s = new ArrayList<String>();
+    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+        button1 = findViewById(R.id.button1);
+        bluetoothAdapter.startDiscovery();
+        // Register for broadcasts when a device is discovered.
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver, filter);
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ListAdapter adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, s);
+                listView.setAdapter(adapter);
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                        String selectedItem = (String) adapterView.getItemAtPosition(position);
+                        String macAdd = nameList.get(selectedItem);
+                        Toast.makeText(MainActivity.this, macAdd, Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
     }
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                Log.i("device", "found");
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                String toShow = deviceName + "\n" + deviceHardwareAddress;
+                if (!nameList.containsValue(deviceHardwareAddress)) {
+                    nameList.put(toShow, deviceHardwareAddress);
+                    s.add(toShow);
+                }
+            }
+        }
+    };
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(receiver);
+    }
+
 }
